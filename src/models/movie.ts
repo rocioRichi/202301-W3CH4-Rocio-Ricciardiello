8af6ca03907ca20828c2de1c0d55f4f7ece1394a
rocioRichi <paragit@gmail.com>
2023-02-02 19:35:12 +0100

    See sonar error

---
+++ src/models/movie.ts
@@ -0,0 +1,20 @@
+export type MovieStructure = {
+  movie: string;
+  creator: string;
+  isBeenSeen: boolean;
+};
+
+export class Task implements TaskStructure {
+  public id: string;
+  public isCompleted: boolean;
+  static generateId() {
+    const aNumbers = new Uint32Array(1);
+    crypto.getRandomValues(aNumbers);
+    return ('000000' + aNumbers[0]).slice(-6);
+  }
+
+  constructor(public name: string, public owner: string) {
+    this.id = Task.generateId();
+    this.isCompleted = false;
+  }
+}
